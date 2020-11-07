5e93ab8b143fe776f14eebf2f88b2db4e723e883
Tom Chestnut <tchestnut85@gmail.com>
2020-11-06 23:24:59 -0500

    added sequelize model for blog posts

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # code-zone
-Code Zone is a developer blog site where user's can publish blog posts as well as comment on others' posts.
+Code Zone is a developer blog site where users can publish blog posts as well as comment on others' posts.
---
+++ models/Blog.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model { }
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        content: {
+            type: DataTypes.TEXT('medium'),
+            allowNull: false
+        },
+        creator_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'User',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Blog'
+    }
+);
+
+module.exports = Blog;
